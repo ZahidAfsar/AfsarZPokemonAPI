e6c3b90b890e5b9deef8263a59b91decfdc2325f
Zahid Afsar <zahidtempz@icloud.com>
2024-01-31 15:51:41 -0800

    favbtn

--- scripts/app.js
+++ scripts/app.js
@@ -18,7 +18,7 @@ let pokemon = "";
 let pokemon2 = "";
 
 const pokemonApi = async (pokemon) => {
-    const promise = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const promise = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}?limit=649`);
     const data = await promise.json();
     console.log(data);
     return data;
@@ -36,6 +36,9 @@ const LocationAPI = async (pokemon2) => {
     return data;
 }
 
+function capitalizeFirstLetter(pokemon) {
+    return pokemon.charAt(0).toUpperCase() + string.slice(1);
+}
 
 inputSearch.addEventListener('keydown', async (event) => {
     if(event.key === "Enter")
@@ -51,7 +54,7 @@ inputSearch.addEventListener('keydown', async (event) => {
         if(!pokemon.length === 0){
             locationText.textContent = `${pokemon2[0].location_area.name}`; 
         }else{
-           locationText.textContent = 'Location Found: N/A';
+           locationText.textContent = 'N/A';
         }
         pokeImg.src = pokemon.sprites.other["official-artwork"].front_default;
     }
@@ -62,11 +65,16 @@ searchBtn.addEventListener('click', async () => {
     {
     console.log("Search btn works");
     pokemon = await pokemonApi(inputSearch.value.toLowerCase());
-    // pokemon2 = await locationApi(inputSearch.value.toLowerCase());
+    pokemon2 = await LocationAPI(inputSearch.value.toLowerCase());
     numText.textContent = pokemon.id;
     nameText.textContent = pokemon.name;
     typeText.textContent = pokemon.types[0].type.name;
-    moveText.textContent = pokemon.move[0].move.name;
+    moveText.textContent = pokemon.moves[0].move.name;
+    if(!pokemon.length === 0){
+        locationText.textContent = `${pokemon2[0].location_area.name}`; 
+    }else{
+       locationText.textContent = 'N/A';
+    }
     pokeImg.src = pokemon.sprites.other["official-artwork"].front_default;
 }
 });
@@ -96,22 +104,45 @@ getFavoritesBtn.addEventListener('click', async () => {
     getFavoritesDiv.textContent = "";
     favorites.map(pokemon => {
 
-        let p = document.createElement('p');
-        p.textContent = pokemon;
-        p.className = "text-lg font-medium text-gray-900 dark:text-white";
+        let div = document.createElement('div');
+        div.textContent = pokemon;
+        div.className = "bg-yellow-300 rounded-lg font-PottaOne"
+        // let p = document.createElement('p');
+        // p.textContent = pokemon;
+        // p.className = "text-lg font-medium text-gray-900 dark:text-white";
         let button = document.createElement('button');
 
         button.type = "button";
         button.textContent = "X"
         button.classList.add(
-            "text-gray-400","bg-transparent","hover:bg-gray-200","hover:text-gray-900","rounded-lg","text-sm","w-8","h-8","justify-end","dark:hover:bg-gray-600","dark:hover:text-white");
+            "text-black","font-PottaOne","bg-transparent","hover:bg-gray-200","hover:text-gray-900","rounded-lg","text-sm","w-8","h-8","flex-1","justify-end","dark:hover:bg-gray-600","dark:hover:text-white");
 
             button.addEventListener('click', () => {
                 removeFromLocalStorage(pokemon);
-                p.remove();
+                div.remove();
             });
-
-        p.append(button);
-        getFavoritesDiv.append(p);
+        div.append(button);
+        getFavoritesDiv.append(div);
     })
 });
+
+
+// pokemon colors 
+// Normal Type: #A8A77A
+// Fire Type: #EE8130
+// Water Type: #6390F0
+// Electric Type: #F7D02C
+// Grass Type: #7AC74C
+// Ice Type: #96D9D6
+// Fighting Type: #C22E28
+// Poison Type: #A33EA1
+// Ground Type: #E2BF65
+// Flying Type: #A98FF3
+// Psychic Type: #F95587
+// Bug Type: #A6B91A
+// Rock Type: #B6A136
+// Ghost Type: #735797
+// Dragon Type: #6F35FC
+// Dark Type: #705746
+// Steel Type: #B7B7CE
+// Fairy Type: #D685AD
